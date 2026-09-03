b4b31568cc0733b86d747c5e47af9e585dcc39ab
Hillel Arnold <hillel.arnold@gmail.com>
2026-09-03 16:08:02 -0400

    remove validation logic

--- src/process_packages.py
+++ src/process_packages.py
@@ -8,39 +8,33 @@ from shutil import copytree, rmtree
 import bagit
 import boto3
 import pandas
-import pymupdf
-from PIL import Image
 
 
 def main(spreadsheet_path, restricted_batch, aws_role_name, aws_bucket_name, restricted_dir, uploaded_dir, root_dir):
     """Main method which calls all other submethods."""
     for current_dir, refid in to_process(spreadsheet_path):
         package_root_path = Path(root_dir, current_dir)
-        package_payload_path = Path(root_dir, current_dir)
         package_type = 'dir'
         if 'Backlog Project' in current_dir:
-            package_payload_path = Path(root_dir, current_dir, 'data')
             package_type = 'bag'
         assert package_root_path.is_dir(), f"Package does not exist at {package_root_path}"
 
         remove_unwanted_files(package_root_path)
 
-        if is_valid_package(package_payload_path):
-            renamed_path = package_root_path
-            if package_type == 'dir':
-                renamed_path = rename_files(package_root_path, refid)
+        if package_type == 'dir':
+            renamed_path = rename_files(package_root_path, refid)
 
-            if restricted_batch:
-                move_to_dir(renamed_path, restricted_dir)
+        if restricted_batch:
+            move_to_dir(renamed_path, restricted_dir)
+        else:
+            if package_type == 'dir':
+                create_bag(str(renamed_path))
+                tarball_path = create_tarball(renamed_path)
             else:
-                if package_type == 'dir':
-                    create_bag(str(renamed_path))
-                    tarball_path = create_tarball(renamed_path)
-                else:
-                    update_bag(str(package_root_path))
-                    tarball_path = create_tarball(package_root_path)
-                upload_package(tarball_path, aws_bucket_name, aws_role_name)
-                move_to_dir(tarball_path, uploaded_dir)
+                update_bag(str(package_root_path))
+                tarball_path = create_tarball(package_root_path)
+            upload_package(tarball_path, aws_bucket_name, aws_role_name)
+            move_to_dir(tarball_path, uploaded_dir)
 
 
 def to_process(spreadsheet_path):
@@ -73,126 +67,6 @@ def move_to_dir(current_path, target_dir):
         rmtree(current_path)
     else:
         current_path.rename(Path(target_dir, current_path.name))
-
-
-def is_valid_package(dir_path):
-    """Validates package structure and assets.
-
-    Args:
-        dir_path (pathlib.Path): path of digitized object to validate.
-    """
-    try:
-        validate_assets(dir_path, dir_path.stem)
-        validate_file_formats(dir_path)
-        validate_ocr(dir_path, dir_path.stem)
-        return True
-    except Exception as e:
-        print(e)
-        return False
-
-
-def validate_directories(dir_path):
-    """Checks for the presence of expected directories.
-
-    Args:
-        dir_path (pathlib.Path): path of digitized assets.
-
-    Raises:
-        FileNotFoundError if not all directories are present.
-    """
-    for dir in ['master', 'master_edited', 'service_edited']:
-        if not (dir_path / dir).is_dir():
-            raise FileNotFoundError(f"Expected directory {dir} is missing")
-
-
-def validate_file_counts(dir_path, current_dir):
-    """Asserts correct number of files is present in each directory.
-
-    Args:
-        dir_path (pathlib.Path): path of digitized assets.
-        current_dir (str): Name of the top-level directory containing assets.
-    """
-    with pymupdf.open(dir_path / 'service_edited' / f'{current_dir}.pdf', filetype='pdf') as document:
-        pdf_page_count = document.page_count
-    master_file_count = len(list((dir_path / 'master').glob(f'{current_dir}*.tif')))
-    master_edited_file_count = len(
-        list((dir_path / 'master_edited').glob(f'{current_dir}*.tif')))
-    if pdf_page_count != master_edited_file_count:
-        raise Exception(
-            f"PDF has {pdf_page_count} pages but found {master_edited_file_count} files in master_edited directory")
-    if master_file_count < master_edited_file_count:
-        raise Exception(
-            f"{master_edited_file_count} files found in master_edited directory but only {master_file_count} in master directory")
-
-
-def validate_file_names(dir_path):
-    """Ensures file names are valid.
-
-    Args:
-        bag_path (pathlib.Path): path of bagit Bag containing assets.
-    """
-    for dir in ['master', 'master_edited', 'service_edited']:
-        for fp in (dir_path / dir).iterdir():
-            if " " in fp.name:
-                raise Exception(f"File name {str(fp)} contains space.")
-
-
-def validate_ocr(dir_path, current_dir):
-    """Ensures there is an OCR layer for each page of the PDF.
-
-    Args:
-        bag_path (pathlib.Path): path of bagit Bag containing assets.
-        current_dir (str): Name of the top-level directory containing assets.
-    """
-    with pymupdf.open(dir_path / 'service_edited' / f'{current_dir}.pdf', filetype='pdf') as document:
-        for page in document:
-            if page.get_text("text"):
-                return True
-    raise Exception(f'No OCR detected in package {current_dir}')
-
-
-def validate_assets(bag_path, current_dir):
-    """Ensures that all expected directories and files are present.
-
-    Args:
-        bag_path (pathlib.Path): path of directory containing assets.
-        current_dir (str): Name of the top-level directory containing assets.
-
-    Raises:
-        AssetValidationError if files delivered do not match expected files.
-    """
-    try:
-        validate_directories(bag_path)
-        validate_file_counts(bag_path, current_dir)
-        validate_file_names(bag_path)
-    except Exception as e:
-        raise Exception(
-            f"Package structure is invalid: {e}") from e
-
-
-def validate_file_characteristics(image_path):
-    with Image.open(image_path) as image:
-        image.load()  # Ensures TIFF is valid
-        assert image.mode in ["L", "RGB"], f"Image format should be RGB or L, got {image.mode}."
-        resolution = image.info.get('dpi', image.info.get('resolution'))
-        assert resolution, "Image does not have embedded resolution information."
-        assert resolution[0] >= 400 and resolution[1] >= 400, f"Image resolution should be at least 400dpi, got {image.info['dpi']}"
-
-
-def validate_file_formats(bag_path):
-    """Ensures that files pass format validation rules.
-
-    Args:
-        bag_path (pathlib.Path): path of bagit Bag containing assets.
-    """
-    for dir in ['master', 'master_edited']:
-        for fp in (bag_path / dir).glob('*.tif'):
-            try:
-                validate_file_characteristics(fp)
-            except AssertionError as e:
-                raise Exception(f"TIFF file does not meet specs: {e}")
-            except Exception as e:
-                raise Exception(f"Invalid TIFF file {str(fp)}: {e}")
 
 
 def remove_unwanted_files(dir_path):
--- tests/test_process_package.py
+++ tests/test_process_package.py
@@ -1,16 +1,11 @@
-import random
 from pathlib import Path
-from shutil import copyfile, copytree, rmtree
+from shutil import copytree, rmtree
 from unittest.mock import patch
 
 import pytest
 
-from src.process_packages import (create_tarball, is_valid_package, main,
-                                  move_to_dir, remove_unwanted_files,
-                                  rename_files, validate_assets,
-                                  validate_directories, validate_file_counts,
-                                  validate_file_formats, validate_file_names,
-                                  validate_ocr)
+from src.process_packages import (create_tarball, main, move_to_dir,
+                                  remove_unwanted_files, rename_files)
 
 DEFAULT_ARGS = [
     '/spreadsheet/path',
@@ -39,7 +34,6 @@ def file_fixture():
 
 @patch('src.process_packages.to_process')
 @patch('src.process_packages.remove_unwanted_files')
-@patch('src.process_packages.is_valid_package')
 @patch('src.process_packages.rename_files')
 @patch('src.process_packages.create_bag')
 @patch('src.process_packages.update_bag')
@@ -48,11 +42,10 @@ def file_fixture():
 @patch('src.process_packages.move_to_dir')
 def test_main_with_dir(
         mock_move, mock_upload, mock_tarball, mock_update_bag, mock_create_bag,
-        mock_rename, mock_is_valid, mock_unwanted, mock_data):
+        mock_rename, mock_unwanted, mock_data):
     current_dir = '/volumes/data/Digitization/123456'
     refid = '1a2b3c4d5e6f7h8i9j10k'
     mock_data.return_value = [(current_dir, refid)]
-    mock_is_valid.return_value = True
     mock_rename.return_value = f'/volumes/data/Digitization/{refid}'
     mock_tarball.return_value = f'/volumes/data/Digitization/{refid}.tar.gz'
     Path(current_dir).mkdir(parents=True)
@@ -61,7 +54,6 @@ def test_main_with_dir(
 
     mock_data.assert_called_once_with('/spreadsheet/path')
     mock_unwanted.assert_called_once_with(Path(current_dir))
-    mock_is_valid.assert_called_once_with(Path(current_dir))
     mock_rename.assert_called_once_with(Path(current_dir), refid)
     mock_create_bag.assert_called_once_with(f'/volumes/data/Digitization/{refid}')
     mock_update_bag.assert_not_called()
@@ -76,7 +68,6 @@ def test_main_with_dir(
 
 @patch('src.process_packages.to_process')
 @patch('src.process_packages.remove_unwanted_files')
-@patch('src.process_packages.is_valid_package')
 @patch('src.process_packages.rename_files')
 @patch('src.process_packages.create_bag')
 @patch('src.process_packages.update_bag')
@@ -85,11 +76,10 @@ def test_main_with_dir(
 @patch('src.process_packages.move_to_dir')
 def test_main_with_bag(
         mock_move, mock_upload, mock_tarball, mock_update_bag, mock_create_bag,
-        mock_rename, mock_is_valid, mock_unwanted, mock_data):
+        mock_rename, mock_unwanted, mock_data):
     refid = '1a2b3c4d5e6f7h8i9j10k'
     current_dir = f'/volumes/data/Digitization/Backlog Project/{refid}'
     mock_data.return_value = [(current_dir, refid)]
-    mock_is_valid.return_value = True
     mock_rename.return_value = current_dir
     mock_tarball.return_value = f'/volumes/data/Digitization/Backlog Project/{refid}.tar.gz'
     Path(current_dir).mkdir(parents=True)
@@ -98,7 +88,6 @@ def test_main_with_bag(
 
     mock_data.assert_called_once_with('/spreadsheet/path')
     mock_unwanted.assert_called_once_with(Path(current_dir))
-    mock_is_valid.assert_called_once_with(Path(current_dir, 'data'))
     mock_rename.assert_not_called()
     mock_create_bag.assert_not_called()
     mock_update_bag.assert_called_once_with(current_dir)
@@ -113,7 +102,6 @@ def test_main_with_bag(
 
 @patch('src.process_packages.to_process')
 @patch('src.process_packages.remove_unwanted_files')
-@patch('src.process_packages.is_valid_package')
 @patch('src.process_packages.rename_files')
 @patch('src.process_packages.create_bag')
 @patch('src.process_packages.update_bag')
@@ -122,11 +110,10 @@ def test_main_with_bag(
 @patch('src.process_packages.move_to_dir')
 def test_main_with_restricted(
         mock_move, mock_upload, mock_tarball, mock_update_bag, mock_create_bag,
-        mock_rename, mock_is_valid, mock_unwanted, mock_data):
+        mock_rename, mock_unwanted, mock_data):
     current_dir = '/volumes/data/Digitization/123456'
     refid = '1a2b3c4d5e6f7h8i9j10k'
     mock_data.return_value = [(current_dir, refid)]
-    mock_is_valid.return_value = True
     mock_rename.return_value = f'/volumes/data/Digitization/{refid}'
     mock_tarball.return_value = f'/volumes/data/Digitization/{refid}.tar.gz'
     Path(current_dir).mkdir(parents=True)
@@ -137,7 +124,6 @@ def test_main_with_restricted(
 
     mock_data.assert_called_once_with('/spreadsheet/path')
     mock_unwanted.assert_called_once_with(Path(current_dir))
-    mock_is_valid.assert_called_once_with(Path(current_dir))
     mock_rename.assert_called_once_with(Path(current_dir), refid)
     mock_create_bag.assert_not_called()
     mock_update_bag.assert_not_called()
@@ -170,147 +156,6 @@ def test_move_to_dir_with_file():
     assert Path(target_dir, 'current.tar.gz').is_file()
     assert not current_file.exists()
     rmtree(target_dir)
-
-
-@patch('src.process_packages.validate_assets')
-@patch('src.process_packages.validate_file_formats')
-@patch('src.process_packages.validate_ocr')
-def test_is_valid_package(mock_ocr, mock_format, mock_assets):
-    dir_path = Path('/path/to/package')
-
-    output = is_valid_package(dir_path)
-
-    assert output
-    mock_assets.assert_called_once_with(dir_path, dir_path.stem)
-    mock_format.assert_called_once_with(dir_path)
-    mock_ocr.assert_called_once_with(dir_path, dir_path.stem)
-
-
-@patch('src.process_packages.validate_assets')
-@patch('src.process_packages.validate_file_formats')
-@patch('src.process_packages.validate_ocr')
-def test_is_valid_package_with_exception(mock_ocr, mock_format, mock_assets):
-    mock_assets.side_effect = Exception()
-    dir_path = Path('/path/to/package')
-
-    output = is_valid_package(dir_path)
-
-    assert not output
-    mock_assets.assert_called_once_with(dir_path, dir_path.stem)
-    mock_format.assert_not_called()
-    mock_ocr.assert_not_called()
-
-
-@patch('src.process_packages.validate_directories')
-@patch('src.process_packages.validate_file_counts')
-@patch('src.process_packages.validate_file_names')
-def test_validate_assets(mock_names, mock_counts, mock_dirs):
-    bag_path = Path('/Path/to/bag')
-    current_dir = 'current_dir'
-
-    validate_assets(bag_path, current_dir)
-
-    mock_dirs.assert_called_once_with(bag_path)
-    mock_counts.assert_called_once_with(bag_path, current_dir)
-    mock_names.assert_called_once_with(bag_path)
-
-
-@patch('src.process_packages.validate_directories')
-@patch('src.process_packages.validate_file_counts')
-@patch('src.process_packages.validate_file_names')
-def test_validate_assets_with_exception(mock_names, mock_counts, mock_dirs):
-    mock_dirs.side_effect = Exception("foo")
-    bag_path = Path('/Path/to/bag')
-    current_dir = 'current_dir'
-
-    with pytest.raises(Exception) as err:
-        validate_assets(bag_path, current_dir)
-    assert str(err.value) == 'Package structure is invalid: foo'
-
-    mock_dirs.assert_called_once_with(bag_path)
-    mock_counts.assert_not_called()
-    mock_names.assert_not_called()
-
-
-def test_validate_directories():
-    dir_path = Path('/current/path')
-    dir_path.mkdir(parents=True)
-    for dir in ['master', 'master_edited', 'service_edited']:
-        (dir_path / dir).mkdir()
-
-    validate_directories(dir_path)  # Happy path
-
-    rmtree(dir_path / 'master_edited')
-
-    with pytest.raises(FileNotFoundError) as err:
-        validate_directories(dir_path)  # With exception
-
-    assert 'master_edited' in str(err.value)
-    rmtree(dir_path)
-
-
-def test_validate_file_counts_master(file_fixture):
-    msg = '2 files found in master_edited directory but only 1 in master directory'
-    files = list(TMP_PATH.glob('master/*'))
-    random.choice(files).unlink()
-
-    with pytest.raises(Exception) as err:
-        validate_file_counts(TMP_PATH, BAG_REFID)
-    assert str(err.value) == msg
-
-
-def test_validate_file_counts_master_edited(file_fixture):
-    msg = 'PDF has 2 pages but found 1 files in master_edited directory'
-    files = list(TMP_PATH.glob('master_edited/*'))
-    random.choice(files).unlink()
-
-    with pytest.raises(Exception) as err:
-        validate_file_counts(TMP_PATH, BAG_REFID)
-    assert str(err.value) == msg
-
-
-def test_validate_file_names_with_space(file_fixture):
-    file = random.choice(list((TMP_PATH / 'master').iterdir()))
-    new_name = file.name.replace("_", " _")
-    file.rename(TMP_PATH / 'master' / new_name)
-
-    with pytest.raises(Exception) as err:
-        validate_file_names(TMP_PATH)
-    assert "contains space" in str(err.value)
-    assert new_name in str(err.value)
-
-
-def test_validate_ocr(file_fixture):
-    validate_ocr(TMP_PATH, BAG_REFID)
-
-    # Assert PDFs without OCR raise error.
-    copyfile(
-        Path("tests", "fixtures", "non-text-searchable.pdf"),
-        TMP_PATH / 'service_edited' / f'{BAG_REFID}.pdf')
-
-    with pytest.raises(Exception) as err:
-        validate_ocr(TMP_PATH, BAG_REFID)
-    assert BAG_REFID in str(err.value)
-
-
-def test_validate_file_formats(file_fixture):
-    """Asserts file formats are validated as expected."""
-    validate_file_formats(TMP_PATH)
-
-
-@patch('src.process_packages.validate_file_characteristics')
-def test_validate_file_formats_with_error(mock_characteristics, file_fixture):
-    error_string = 'This is an error!'
-    mock_characteristics.side_effect = AssertionError(error_string)
-    with pytest.raises(Exception) as err:
-        validate_file_formats(TMP_PATH)
-    assert error_string in (str(err.value))
-
-    error_string = 'This is a different error!'
-    mock_characteristics.side_effect = Exception(error_string)
-    with pytest.raises(Exception) as err:
-        validate_file_formats(TMP_PATH)
-    assert error_string in (str(err.value))
 
 
 def test_remove_unwanted_files(file_fixture):
